11bb9824b497eb81dc842fd6dfee9ffb5d1a8f3e
Chidrup-18 <01fe24bcs290@kletech.ac.in>
2026-04-19 02:54:08 +0530

    Update ocr_module.py

--- modules/ocr_module.py
+++ modules/ocr_module.py
@@ -1,33 +1,22 @@
-# ocr_module.py
-
 import easyocr
 
-reader_cache = {}
+_reader_cache = {}
 
 def get_reader(langs):
     key = "_".join(langs)
-
-    if key not in reader_cache:
-        reader_cache[key] = easyocr.Reader(langs, gpu=False)
-
-    return reader_cache[key]
-
+    if key not in _reader_cache:
+        _reader_cache[key] = easyocr.Reader(langs, gpu=False)
+    return _reader_cache[key]
 
 def extract_text(image_path, doc_type):
-
     if doc_type == "aadhaar":
-        langs = ['en', 'hi']
-    elif doc_type == "pan":
-        langs = ['en']
+        langs = ["en", "hi"]
     elif doc_type == "sslc":
-        langs = ['en', 'kn']
+        langs = ["en", "kn"]
     else:
-        langs = ['en']
+        langs = ["en"]
 
     reader = get_reader(langs)
-
-    results = reader.readtext(image_path)
-
-    texts = [r[1] for r in results]
-
-    return texts, results
+    result = reader.readtext(image_path)
+    texts = [r[1] for r in result]
+    return texts, result
